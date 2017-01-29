9104165220f555790fbbccb8724a71ae6ba75b15
jorisgls <contact.jorisfr@gmail.com>
2017-01-29 08:55:01 +0100

    modif

--- interviews.php
+++ interviews.php
@@ -16,11 +16,8 @@ $pageid = "interviews";
 <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/bootstrap.min.js"></script>
-<link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/redmond/jquery-ui.css" rel="stylesheet" />
-<link href="/bootstrap.min.css" rel="stylesheet">
-<link href="/bootstrap.css" rel="stylesheet">
+<script src="/bootstrap/min.js"></script>
+
 
 
 
@@ -39,10 +36,9 @@ $pageid = "interviews";
 
 <link rel="shortcut icon" href="<?php echo URL; ?>/web/images/favicon.ico" type="image/vnd.microsoft.icon">
 <link href="<?php echo URL; ?>/web/styles/style.css" rel="stylesheet" type="text/css">
-<link href="https://bootswatch.com/lumen/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="<?php echo URL; ?>/web/styles/bootstrap-social.css" rel="stylesheet" type="text/css">
 <link href="<?php echo URL; ?>/web/styles/font-awesome.css" rel="stylesheet" type="text/css">
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300normal,300italic,400normal,400italic,600normal,600italic,700normal,700italic,800normal,800italic&amp;subset=all" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" type="text/css">
 
